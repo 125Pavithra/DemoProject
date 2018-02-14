b1c7b39edaa54dda082ff45fea26a74da7585851
Saju Mathew <smathew@healthgrades.com>
2018-02-14 17:23:52 +0530

    mcv

---
+++ Controllers/DemoPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoProject.Controllers
+{
+    public class DemoPageController : Controller
+    {
+        // GET: DemoPage
+        public string Index()
+        {
+            return "Hello Welcome to MVC";
+        }
+    }
+}
